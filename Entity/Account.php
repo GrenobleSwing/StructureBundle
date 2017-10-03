745c4b2fb6145fb3cfef0a01e3a8f9393e00a17c
Julien Billoudet <julien.billoudet@gmail.com>
2017-10-03 08:51:34 +0200

    Correction for proper image upload.

--- Entity/Account.php
+++ Entity/Account.php
@@ -435,6 +435,13 @@ class Account
     public function setImageFile(File $image = null)
     {
         $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
         return $this;
     }
 
